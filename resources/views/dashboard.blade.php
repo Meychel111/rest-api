5a45138ecb3e1ca42df3778679f03cfbc5543868
Meychel111 <meychel111@gmail.com>
2025-06-25 22:26:26 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+@section('content')
+    <h3>Dashboard</h3>
+    <p>Selamat Datang di sistem informasi data mahasiswa. silahkan pilih menu di sebelah kiri untuk mengelola data mahasiswa
+    </p>
+@endsection
